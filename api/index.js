3605c85ba4f7dba3d92c048e718de07786d380b7
Phezzant <phezzant@gmail.com>
2025-10-01 10:38:06 +0300

    Quick patch again adding fs dependency

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fs = require('fs');
 const Database = require('better-sqlite3');
 
 const app = express();
